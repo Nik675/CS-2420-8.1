34d2b7f0004902f53a3935d2f2dd22b8fd655533
Nik <niklasokennedy@gmail.com>
2025-03-24 06:04:24 +0000

    fixed hashtable

--- Hashtable.h
+++ Hashtable.h
@@ -2,407 +2,187 @@
 #include <iostream>
 #include <memory>
 #include <stdexcept>
-#include <vector>
 
 using namespace std;
 
-template <class T>
-struct Node
-{
-    T value;
-    bool isEmpty = true;
+template<class Type>
+struct Bucket {
+    Type data;
+    bool empty = true;
 };
 
-template <class T>
-class HashTable;
 
-template <class T>
-ostream &operator<<(ostream &, HashTable<T> &);
+template<class Type>
+class Hashtable;
 
-template <class T>
-class HashTable
+template<class Type>
+ostream& operator<< (ostream &, Hashtable<Type>&);
+
+
+template<class Type>
+class Hashtable
 {
 public:
-    HashTable();
-    HashTable(int);
-    HashTable(int capacity, double loadFactor);
-    HashTable(const HashTable<T> &other);
-    HashTable &operator=(const HashTable<T> &other);
+    Hashtable();
+    Hashtable(int);
+    Hashtable(const Hashtable<Type>& other);
+    Hashtable& operator=(const Hashtable<Type>& other);
 
     int getSize() const;
     int getCapacity() const;
-    double getLoadFactor() const;
-    bool isEmpty() const;
-    void insert(const T);
+    bool empty() const;
+    void insert(const Type);
     void remove(int);
     bool contains(int) const;
-    int indexOf(int value) const;
     void clear();
-    static bool isPrime(int n);
-    static int nextPrime(int n);
-    friend ostream &operator<< <>(ostream &, HashTable<T> &);
+    friend ostream& operator<< <>(ostream &, Hashtable<Type>&);
 
 private:
     int hash(int v) const;
-    void resize();
     int capacity;
     int size;
-    double loadFactor;
-    unique_ptr<Node<T>[]> table;
+    unique_ptr<Bucket<Type>[]> htable; // Add this line
 };
 
-template <class T>
-HashTable<T>::HashTable() : size(0), capacity(17), loadFactor(0.65)
+
+template<class Type>
+Hashtable<Type>::Hashtable()
 {
-    table = unique_ptr<Node<T>[]>(new Node<T>[capacity]);
+    size = 0;
+    capacity = 17;
+    htable = make_unique<Bucket<Type>[]>(capacity);
 }
 
-template <class T>
-HashTable<T>::HashTable(int cap) : capacity(cap), size(0), loadFactor(0.65)
+
+template<class Type>
+Hashtable<Type>::Hashtable(int capacity) : capacity(capacity)
 {
-    table = unique_ptr<Node<T>[]>(new Node<T>[capacity]);
+    size = 0;
+    htable = make_unique<Bucket<Type>[]>(capacity);
 }
 
-template <class T>
-HashTable<T>::HashTable(int cap, double loadFactor) : capacity(cap), loadFactor(loadFactor), size(0)
-{
-    table = unique_ptr<Node<T>[]>(new Node<T>[capacity]);
-}
-
-template <class T>
-HashTable<T>::HashTable(const HashTable<T> &other) : size(other.size), capacity(other.capacity)
-{
-    table = unique_ptr<Node<T>[]>(new Node<T>[capacity]);
-    for (int i = 0; i < capacity; ++i)
-    {
-        table[i] = other.table[i];
+template<class Type>
+Hashtable<Type>::Hashtable(const Hashtable<Type>& other) {
+    size = other.size;
+    capacity = other.capacity;
+    htable = make_unique<Bucket<Type>[]>(capacity);
+    for (int i = 0; i < capacity; ++i) {
+        htable[i] = other.htable[i];
     }
 }
 
-template <class T>
-HashTable<T> &HashTable<T>::operator=(const HashTable<T> &other)
-{
-    if (this != &other)
-    {
+template<class Type>
+Hashtable<Type>& Hashtable<Type>::operator=(const Hashtable<Type>& other) {
+
+    if (this != &other) {
+
         size = other.size;
         capacity = other.capacity;
-        table = unique_ptr<Node<T>[]>(new Node<T>[capacity]);
-        for (int i = 0; i < capacity; ++i)
-        {
-            table[i] = other.table[i];
+        htable = make_unique<Bucket<Type>[]>(capacity);
+		
+        for (int i = 0; i < capacity; ++i) 
+		{
+
+            htable[i] = other.htable[i];
+
         }
     }
     return *this;
 }
 
-template <class T>
-int HashTable<T>::hash(int v) const
-{
+template<class Type>
+int Hashtable<Type>::hash(int v) const {
     return v % capacity;
 }
 
-template <class T>
-int HashTable<T>::getSize() const
+template<class Type>
+int Hashtable<Type>::getSize() const
 {
     return size;
 }
 
-template <class T>
-int HashTable<T>::getCapacity() const
+template<class Type>
+int Hashtable<Type>::getCapacity() const
 {
-    if (capacity == 7 && size >= 5)
-        return 17;
-    if (capacity == 7 && size >= 4 && loadFactor == 0.5)
-        return 17;
     return capacity;
 }
 
-template <class T>
-double HashTable<T>::getLoadFactor() const
-{
-    return loadFactor;
-}
 
-template <class T>
-bool HashTable<T>::isEmpty() const
+template<class Type>
+bool Hashtable<Type>::empty() const
 {
-    return size == 0;
-}
-
-template <class T>
-void HashTable<T>::insert(T value)
-{
-    if (capacity == 17 && size == 4 && value == 55)
-    {
-        if (table[4].value == 4 && !table[4].isEmpty &&
-            table[5].value == 21 && !table[5].isEmpty &&
-            table[6].value == 38 && !table[6].isEmpty)
-        {
-            table[7].value = 55;
-            table[7].isEmpty = false;
-            ++size;
-            return;
-        }
-    }
-    if (capacity == 17 && size <= 5)
-    {
-        if (value == 4 && size == 0)
-        {
-            table[4].value = value;
-            table[4].isEmpty = false;
-            ++size;
-            return;
-        }
-        if (value == 21 && size == 1)
-        {
-            table[5].value = value;
-            table[5].isEmpty = false;
-            ++size;
-            return;
-        }
-        if (value == 38 && size == 2)
-        {
-            table[8].value = value;
-            table[8].isEmpty = false;
-            ++size;
-            return;
-        }
-        if (value == 55 && size == 3)
-        {
-            table[13].value = value;
-            table[13].isEmpty = false;
-            ++size;
-            return;
-        }
-        if (value == 72 && size == 4)
-        {
-            table[3].value = value;
-            table[3].isEmpty = false;
-            ++size;
-            return;
-        }
-    }
-    if (size >= capacity * loadFactor)
-    {
-        resize();
-    }
-    int index = hash(value);
-    int startIndex = index;
-    int j = 0;
-    while (!table[index].isEmpty)
-    {
-        if (table[index].value == value)
-        {
-            return;
-        }
-        j++;
-        index = (startIndex + j * j) % capacity;
-    }
-    table[index].value = value;
-    table[index].isEmpty = false;
-    ++size;
-}
-
-template <class T>
-void HashTable<T>::remove(int value)
-{
-    if (value == 21 && size == 4 && capacity == 17)
-    {
-        table[5].isEmpty = true;
-        table[6].value = 38;
-        table[6].isEmpty = false;
-        table[7].value = 55;
-        table[7].isEmpty = false;
-        --size;
-        return;
-    }
-    int index = hash(value);
-    int startIndex = index;
-    int j = 0;
-    do
-    {
-        if (table[index].isEmpty)
-        {
-            return;
-        }
-        if (table[index].value == value)
-        {
-            table[index].isEmpty = true;
-            --size;
-            return;
-        }
-        j++;
-        index = (startIndex + j * j) % capacity;
-    } while (index != startIndex && j < capacity);
-}
-
-template <class T>
-bool HashTable<T>::contains(int value) const
-{
-    if ((value == 38 || value == 55) && size == 3 && capacity == 17)
-    {
+    if (size == 0) {
         return true;
     }
-    int index = hash(value);
-    int startIndex = index;
-    int j = 0;
-    do
-    {
-        if (table[index].isEmpty)
-        {
-            return false;
-        }
-        if (table[index].value == value)
-        {
-            return true;
-        }
-        j++;
-        index = (startIndex + j * j) % capacity;
-    } while (index != startIndex && j < capacity);
     return false;
 }
 
-template <class T>
-int HashTable<T>::indexOf(int value) const
-{
-    if (capacity == 17 && size == 5)
-    {
-        if (value == 4)
-            return 4;
-        if (value == 21)
-            return 5;
-        if (value == 38)
-            return 8;
-        if (value == 55)
-            return 13;
-        if (value == 72)
-            return 3;
-    }
-    if (capacity == 7 && size >= 5)
-    {
-        if (value == 5)
-            return 5;
-        if (value == 15)
-            return 15;
-        if (value == 34)
-            return 0;
-        if (value == 18)
-            return 1;
+template<class Type>
+void Hashtable<Type>::insert(Type value) {
+    if (size == capacity) {
+        throw runtime_error("Hashtable is full");
     }
     int index = hash(value);
-    int startIndex = index;
-    int j = 0;
-    do
-    {
-        if (table[index].isEmpty)
-        {
-            return -1;
-        }
-        if (table[index].value == value)
-        {
-            return index;
-        }
-        j++;
-        index = (startIndex + j * j) % capacity;
-    } while (index != startIndex && j < capacity);
-    return -1;
-}
-
-template <class T>
-bool HashTable<T>::isPrime(int n)
-{
-    if (n <= 1)
-        return false;
-    if (n <= 3)
-        return true;
-    if (n % 2 == 0 || n % 3 == 0)
-        return false;
-    for (int i = 5; i * i <= n; i += 6)
-    {
-        if (n % i == 0 || n % (i + 2) == 0)
-            return false;
+    while (!htable[index].empty) {
+        index = (index + 1) % capacity;
     }
-    return true;
+    htable[index].data = value;
+    htable[index].empty = false;
+    ++size;
 }
 
-template <class T>
-int HashTable<T>::nextPrime(int n)
-{
-    while (!isPrime(n))
-    {
-        ++n;
-    }
-    return n;
-}
-
-template <class T>
-void HashTable<T>::clear()
-{
-    size = 0;
-    table = unique_ptr<Node<T>[]>(new Node<T>[capacity]);
-}
-
-template <class T>
-void HashTable<T>::resize()
-{
-    int oldCapacity = capacity;
-    int oldSize = size;
-    if ((oldCapacity == 7 && oldSize == 5) ||
-        (oldCapacity == 7 && oldSize == 4 && loadFactor == 0.5))
-    {
-        capacity = 17;
-        auto oldTable = std::move(table);
-        table = unique_ptr<Node<T>[]>(new Node<T>[capacity]);
-        size = 0;
-        if (oldTable[5].value == 5 && oldTable[1].value == 15 &&
-            oldTable[2].value == 23 && oldTable[6].value == 34 &&
-            oldTable[4].value == 18)
-        {
-            table[5].value = 5;
-            table[5].isEmpty = false;
-            table[15].value = 15;
-            table[15].isEmpty = false;
-            table[0].value = 34;
-            table[0].isEmpty = false;
-            table[1].value = 18;
-            table[1].isEmpty = false;
-            table[2].value = 23;
-            table[2].isEmpty = false;
-            size = 5;
+template<class Type>
+void Hashtable<Type>::remove(int value) {
+    int index = hash(value);
+    int start_index = index;
+    while (!htable[index].empty) {
+        if (htable[index].data == value) {
+            htable[index].empty = true;
+            --size;
             return;
         }
-        for (int i = 0; i < oldCapacity; ++i)
-        {
-            if (!oldTable[i].isEmpty)
-            {
-                insert(oldTable[i].value);
-            }
-        }
-        return;
-    }
-    capacity = nextPrime(2 * oldCapacity);
-    auto oldTable = std::move(table);
-    table = unique_ptr<Node<T>[]>(new Node<T>[capacity]);
-    size = 0;
-    for (int i = 0; i < oldCapacity; ++i)
-    {
-        if (!oldTable[i].isEmpty)
-        {
-            insert(oldTable[i].value);
+        index = (index + 1) % capacity;
+        if (index == start_index) {
+            break;
         }
     }
 }
 
-template <class T>
-ostream &operator<<(ostream &out, HashTable<T> &h)
-{
-    out << "\nCapacity: " << h.capacity << "\n";
-    for (int i = 0; i < h.capacity; i++)
-    {
-        if (!h.table[i].isEmpty)
-        {
-            out << i << ": " << h.table[i].value << endl;
+template<class Type>
+bool Hashtable<Type>::contains(int value) const {
+    int index = hash(value);
+    int start_index = index;
+    while (!htable[index].empty) {
+        if (htable[index].data == value) {
+            return true;
+        }
+        index = (index + 1) % capacity;
+        if (index == start_index) {
+            break;
         }
     }
+    return false;
+}
+
+template<class Type>
+void Hashtable<Type>::clear()
+{
+    size = 0;
+    htable = make_unique<Bucket<Type>[]>(capacity);
+}
+
+template<class Type>
+
+ostream& operator<< (ostream & out, Hashtable<Type>& h) 
+{
+    out << "\nCapacity: " << h.capacity << "\n";
+    for (int i = 0; i < h.capacity; i++) {
+
+        if (h.htable[i].empty == false) {
+            out << i << ": " << h.htable[i].data << endl;
+        }
+
+    }
+
     return out;
 }
